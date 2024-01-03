f90a65ecdd4c8539c7d07c53ea1c8fbd3896dfea
samuel-bd <samuely@frisseblikken.com>
2024-01-03 11:25:13 +0700

    feat(chat): username and content states

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,12 +1,20 @@
 'use client'
+
+import { useState } from "react"
 import MessageList from "./MessageList"
 
 const Chat = () => {
+    const [username, setUsername] = useState<string>('');
+    const [content, setContent] = useState<string>('');
+
     return (
         <div>
             <form>
-                <input type="text" value="username" required placeholder="Your username" />
-                <input type="text" value="username" required placeholder="Your username" />
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required placeholder="Your username" />
+                {' '}
+                says:
+                {' '}
+                <input type="text" value={content} onChange={(e) => setContent(e.target.value)} required placeholder="Your message" />
                 <input type="submit" />
             </form>
             <MessageList />
