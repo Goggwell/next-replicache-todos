b40d02579d3886df2f4d555d4b7e6beeb5c48fde
samuel-bd <samuely@frisseblikken.com>
2024-01-03 11:46:39 +0700

    feat(replicache): createmessage mutator

--- lib/constructor.ts
+++ lib/constructor.ts
@@ -1,12 +1,36 @@
 'use client'
 
-import { Replicache } from "replicache"
+import { Replicache, type WriteTransaction } from "replicache"
 
-const getReplicache = () => {
-    const rep = new Replicache({
-        name: "chat-rep",
-        licenseKey: process.env.REPLICACHE_LICENSE_KEY ?? 'l00000000000000000000000000000001', // replicache tutorial license key
-    })
+interface IMessage {
+    id: string;
+    from: string;
+    content: string;
+    order: number;
+}
+
+const getReplicache = (): Replicache<{
+    createMessage: (tx: WriteTransaction, {
+        id, from, content, order,
+    }: IMessage) => Promise<void> }> | null => {
+    const rep = typeof window !== 'undefined' ? 
+        new Replicache({
+            name: "chat-rep",
+            licenseKey: process.env.REPLICACHE_LICENSE_KEY ?? 'l00000000000000000000000000000001', // replicache tutorial license key
+            mutators: {
+                async createMessage(
+                    tx: WriteTransaction,
+                    {
+                        id, from, content, order,
+                    }: IMessage,
+                ) {
+                    await tx.set(`message/${id}`, {
+                        from, content, order,
+                    })
+                }
+            }
+        })
+        : null
 
     return rep
 }
